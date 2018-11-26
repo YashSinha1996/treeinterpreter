6ec19a9684ec7aeee9272aa6ca60be6e03749a35
Yash <yash@dishq.in>
2018-11-26 12:19:32 +0530

    parralel more

--- treeinterpreter/treeinterpreter.py
+++ treeinterpreter/treeinterpreter.py
@@ -173,7 +173,7 @@ def _predict_tree(model, X):
 
     print(unique_leaves.shape, len(leaves))
 
-    contribs_total = Parallel(n_jobs=180)(delayed(_get_tree_contribs)
+    contribs_total = Parallel(n_jobs=360)(delayed(_get_tree_contribs)
                                                            (values_list, feature_index, leaf_to_path[leaf], line_shape)
                                                            for leaf in unique_leaves)
     # for row, leaf in enumerate(unique_leaves):
@@ -207,8 +207,8 @@ def _predict_forest(model, X):
     for tree in model.estimators_:
         contribution = _predict_tree(tree, X)
         contributions += contribution
-        print(contributions, contribution, num_trees)
-        input()
+        # print(contributions, contribution, num_trees)
+        # input()
 
     contributions = contributions / num_trees
 
